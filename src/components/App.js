28729eebac111e5e8320513670e3ec99e2ac0994
S <>
2022-04-09 04:12:10 +0300

    main page

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,17 @@
 import "../styles/index.scss"
+import blob1 from "../images/blob1.png"
+import blob2 from "../images/blob2.png"
 
 function App() {
-    return <h1>Quizz</h1>
+    return (
+        <main className="intro-page">
+            <h1>Quizzical</h1>
+            <h3>Some description if needed</h3>
+            <img src={blob1} className="blob blob--yellow" />
+            <img src={blob2} className="blob blob--blue" />
+            <button className="button">Start quiz</button>
+        </main>
+    )
 }
 
 export default App
